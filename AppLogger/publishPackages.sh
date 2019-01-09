02f408fa40862ff17587a3f0d1843b0488c2da41
bazoki <32161145+bazoki@users.noreply.github.com>
2019-01-09 15:28:42 +0000

    added config

---
+++ AppLogger/publishPackages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# DESCRIPTION: A simple shell script designed to fetch the latest version
+# of the artifacts credential provider plugin for dotnet and
+# install it into $HOME/.nuget/plugins.
+# SEE: https://github.com/Microsoft/artifacts-credprovider/blob/master/README.md
+
+for i in `find . -name "*.nupkg" -type f`; do
+    echo "$i"
+done
