5ae90cb6fb3caa3d8114ace6781eca5aeee5a038
bazoki <32161145+bazoki@users.noreply.github.com>
2019-01-09 13:36:38 +0000

    added config

--- AppLogger/installcredprovider.sh
+++ AppLogger/installcredprovider.sh
@@ -1,3 +1,8 @@
 ﻿#!/usr/bin/env bash
+# DESCRIPTION: A simple shell script designed to fetch the latest version
+# of the artifacts credential provider plugin for dotnet and
+# install it into $HOME/.nuget/plugins.
+# SEE: https://github.com/Microsoft/artifacts-credprovider/blob/master/README.md
+
 
 echo "Hello world"
