1b4bacf4ed193548e200f48a75035d4a6a0707cf
bazoki <32161145+bazoki@users.noreply.github.com>
2019-01-10 13:05:52 +0000

    added config

--- AppLogger/publishPackages.sh
+++ AppLogger/publishPackages.sh
@@ -7,5 +7,19 @@
 cp  AppLogger/NuGet.config .
 
 for i in `find . -name "*.nupkg" -type f`; do
-	dotnet nuget push "$i" -s test-feed -k anything
+	
+	output=$(output=dotnet nuget push "$i" -s test-feed -k anything; 2>&1)
+	
+	ret=$?
+
+	if [[ $ret -eq 0 ]]; then
+		echo OK
+	else
+		if [[ $output == *'Conflict - The feed already contains'* ]]; then
+			 echo OK
+		else
+			 echo FAIL
+		fi
+	fi
+
 done
